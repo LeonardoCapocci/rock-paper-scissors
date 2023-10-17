ee4e77edcd60fbdd2bc0c2a16bb6d8a1755ca6f8
LeonardoCapocci <leocap2001@gmail.com>
2023-10-17 13:22:39 -0700

    removed prompt for players selection temporarily

--- app.js
+++ app.js
@@ -1,84 +1,73 @@
 function rockPaperScissors() {
     let int = Math.floor(Math.random() * 3)
+    let response = ''
     if (int == 0) {
-        let response = "rock"
-        return response
+        response = "rock"
     }
     else if (int == 1) {
-        let response = "paper"
-        return response
+        response = "paper"
     }
     else if (int == 2) {
-        let response = "scissors"
-        return response
+        response = "scissors"
     }
-    else {
-        let response = "something's wrong"
-        return response
-    }
+    return response
 }
 
 function playRound() {
-    let playerSelection = prompt("rock, paper, or scissors?")
+    let playerSelection = "rock"
     let computerSelection = rockPaperScissors()
+    let result = ''
     if (playerSelection == "rock") {
         if (computerSelection == "rock") {
-            let result = "TIE"
-            return result
+            result = "TIE"
         }
         if (computerSelection == "paper") {
-            let result = "LOSS"
-            return result
+            result = "LOSS"
         }
         if (computerSelection == "scissors") {
-            let result = "WIN"
-            return result
+            result = "WIN"
         }
     }
     else if (playerSelection == "paper") {
         if (computerSelection == "rock") {
-            let result = "WIN"
-            return result
+            result = "WIN"
         }
         if (computerSelection == "paper") {
-            let result = "TIE"
-            return result
+            result = "TIE"
         }
         if (computerSelection == "scissors") {
-            let result = "LOSS"
-            return result
+            result = "LOSS"
         }
     }
     else if (playerSelection == "scissors") {
         if (computerSelection == "rock") {
-            let result = "LOSS"
-            return result
+            result = "LOSS"
         }
         if (computerSelection == "paper") {
-            let result = "WIN"
-            return result
+            result = "WIN"
         }
         if (computerSelection == "scissors") {
-            let result = "TIE"
-            return result
+            result = "TIE"
         }
     }
+    return result
 }
 
 function game() {
     let playerWins = 0
     let computerWins = 0
+    let roundResult = ''
     while (playerWins < 3 && computerWins < 3) {
-        result = playRound()
-        if (result == "WIN") {
+        roundResult = playRound()
+        if (roundResult == "WIN") {
             playerWins++
         }
-        else if (result == "LOSS") {
+        else if (roundResult == "LOSS") {
             computerWins++
         }
         else {
         }
-        console.log(result)
+        console.log(roundResult)
     }
     if (playerWins == 3) {
         console.log("You win the game!")
